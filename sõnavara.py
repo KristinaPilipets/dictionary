f59a6b4340867f12635f246a09ec5a315ba3951c
opilane <opilane@HP-CZC6297BB1>
2021-12-02 09:39:52 +0200

    dictionary

--- sõnavara.py
+++ sõnavara.py
@@ -1,1 +1,16 @@
+Capitals={}
+Capitals = dict()
+Capitals['Russia'] = 'Moscow'
+Capitals['Ukraine'] = 'Kiev'
+Capitals['USA'] = 'Washington'
+Countries = ['Russia', 'France', 'USA', 'Russia']
+for country in Countries:
+	if country in Capitals:
+		print('Столица страны ' + country + ': ' + Capitals[country])
+	else:
+		print('В базе нет страны c названием ' + country)
+
+dictionary = {'персона': 'человек','марафон': 'гонка бегунов длиной около 26 миль','противостоять': 'оставаться сильным, несмотря на давление','бежать': 'двигаться со скоростью'}
+
+gender_dict = {0: 'муж', 1: 'жен'}
 
