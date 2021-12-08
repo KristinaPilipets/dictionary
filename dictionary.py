9b35eff1e482cb3eb406c5a8cf0ae5eb6d43faac
opilane <opilane@HP-CZC6297BB1>
2021-12-08 12:31:11 +0200

    добавлена фун исправления в словаре и первая часть фун теста

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,26 @@
 from modul import*
 Capitals={}
-c="1"
 with open("Cap.txt") as f:
 	for i in f:
 		k,v=i.strip().split("-")
 		Capitals[k.strip()]=v.strip()
-print(Capitals.items())
 
 while True:
-	c=input("Otsing sõna sõnavaras - 1, ")
+	
+	c=input("Otsing sõna sõnavaras - 1, muutma sõna või sõna tähendus - 2, test - 3")
 	if c=="1":
-		find_word()
-	elif c=="2"
+		ans,a=find_word(Capitals)
+		print(ans)
+		if ans=="See sõna ei olema":
+			b=input("Kas te tahate lisa sõna sõnavars, 1-jah 2-ei >>> ")
+			if b=="1":
+				Capitals=add_word(Capitals,a)
+			else:
+				pass
+		else:
+			pass
+	elif c=="2":
+		Capitals=change_smth(Capitals)
+	elif c=="3":
+		testingknoledge(Capitals)
+		
--- modul.py
+++ modul.py
@@ -1,4 +1,6 @@
-def find_word(Capitals:dict):
+from random import*
+
+def find_word(Capitals:dict)->str:
 	"""Otsing sõna sõnavaras
 	"""
 	a=input("Siseta riigi nimi või pealinn inglise keeles >>> ")
@@ -11,5 +13,34 @@ def find_word(Capitals:dict):
 			break
 		else:
 			ans="See sõna ei olema"
-	print(ans)
+	return ans,a
 
+def add_word(Capitals:dict, a:str)->dict:
+	b=input("Siseta sõna tähendus >>> ")
+	Capitals.update({a:b})
+	return Capitals
+
+def change_smth(Capitals:dict)->dict:
+	a=input("Siseta sõna >>> ")
+	c=input("Siseta uus tähendus >>> ")
+	Capitals.update({a.title():c.title()})
+	return Capitals
+
+def testingknoledge(Capitals:dict):
+	b=input("Kas te tahate testida Pealinns või Riigid? 1-pealinn, 2-riigid")
+	if b=="1":
+		a=0
+		capitals=list(Capitals)
+		print(capitals)
+		for i in range (0,11): 
+			word=choice(capitals)
+			print(word)
+			ans=input(str(i)+" Siseta pealinn >>> ")
+			if ans in Capitals.values():
+				print("Õige!")
+				a+=1
+			else:
+				print("Vale!")
+		result=a*100/10
+	print("Teie resultat on "+str(result)+"%")
+	#ans=input("Siseta riig >>> ")
