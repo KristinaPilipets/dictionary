c3f6095d299b114de41c065fd093fed1a5a1604b
opilane <opilane@HP-CZC6297BB1>
2021-12-09 09:10:31 +0200

    +comments

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,19 @@
 from modul import*
 Capitals={}
 with open("Cap.txt") as f:
-	for i in f:
-		k,v=i.strip().split("-")
-		Capitals[k.strip()]=v.strip()
+	for i in f: # создаем цикл по кол-ву строк
+		k,v=i.strip().split("-") #отделяем слова на строчке в строчке по знаку "-"
+		Capitals[k.strip()]=v.strip() # добавляем в словарь
 
 while True:
-	c=input("Otsing sõna sõnavaras - 1, muutma sõna tähendus - 2, test - 3")
+	c=input("Otsing sõna sõnavaras - 1, muutma sõna tähendus - 2, test - 3, välja - 4")
 	if c=="1":
 		ans,a=find_word(Capitals)
 		print(ans)
-		if ans=="See sõna ei olema":
+		if ans=="See sõna ei olema": # если слова нет в словаре спрашиваем хочет ли пользователь добавить это слово
 			b=input("Kas te tahate lisa sõna sõnavars, 1-jah 2-ei >>> ")
-			if b=="1":
-				Capitals=add_word(Capitals,a)
+			if b=="1": # если да 
+				Capitals=add_word(Capitals,a) # используем функцию добавления в словарь и из поиска слова берем переменную а которая будет ключем
 			else:
 				pass
 		else:
@@ -22,7 +22,9 @@ while True:
 		Capitals=change_smth(Capitals)
 	elif c=="3":
 		result=testingknoledge(Capitals)
-		print("Sinu resultat on "+str(result)+"%")
+		print("Sinu resultat on "+str(result)+"%") # на экране показываем ответ
+	elif c=="4": # заканчиваем цикл 
+		break
 	else:
 		print("See funktioon ei olema")
 		
--- modul.py
+++ modul.py
@@ -5,62 +5,62 @@ def find_word(Capitals:dict)->str:
 	"""
 	a=input("Siseta riigi nimi või pealinn inglise keeles >>> ")
 	for key, value in Capitals.items(): #проверяем каждое значение вместе со словами 
-		if a.title()==value:
-			ans=(a+" / "+key)
+		if a.title()==value: #смотрим если введенная переменная это значение
+			ans=( a.title()+" / "+key ) # к переменной добавляем ключ
 			break
-		elif a.title()==key:
-			ans=(a+" / "+value)
+		elif a.title()==key: #смотрим если введенная переменная это ключ
+			ans=( a.title()+" / "+value ) # к переменной добавляем значение
 			break
 		else:
-			ans="See sõna ei olema"
+			ans="See sõna ei olema" #если слова не существует в словаре
 	return ans,a
 
 def add_word(Capitals:dict, a:str)->dict:
 	"""Lisama sõna sõnavaras
 	"""
-	b=input("Siseta sõna tähendus >>> ")
-	Capitals.update({a:b.title()})
+	b=input("Siseta sõna tähendus >>> ") #просим ввести значение слова, мы знаем ключ из прошлой функции по поиску
+	Capitals.update({a.title():b.title()}) # добавляем в словарь
 	return Capitals
 
 def change_smth(Capitals:dict)->dict:
 	"""Muutma midagi sõnavaras
 	"""
-	a=input("Siseta sõna >>> ")
+	a=input("Siseta sõna, milline tähendus te tahate muuta >>> ")
 	c=input("Siseta uus tähendus >>> ")
-	Capitals.update({a.title():c.title()})
-	return Capitals
+	Capitals.update({a.title():c.title()}) #обновляем значение слова по ключу
+	return Capitals #возращаем измененый словарь
 
 def testingknoledge(Capitals:dict)->float:
 	"""Küsida mis ta tahab testida ja küsida 10 küsimust
 	"""
-	result=0
-	country=list(Capitals)
-	nocount={}
+	result=0 #переменную приравниваем к нулю чтобы при не той выборе функции результат был ноль
+	country=list(Capitals) # делаем из словаря список
+	nocount={} 
 	with open("Cap.txt") as f:
 		for i in f:
-			v,k=i.strip().split("-")
+			v,k=i.strip().split("-") #создаем словарь наоборот где столицы это ключи, а страны - значения
 			nocount[k.strip()]=v.strip()
 	capitals=list(nocount)
-	b=input("Kas te tahate testida Pealinns või Riigid? 1-pealinn, 2-riigid, 3-riigid ja pealinnad")
+	b=input("Kas te tahate testida Pealinns või Riigid? 1-pealinn (10 küsimused), 2-riigid (10 küsimused), 3-riigid ja pealinnad (15 küsimused)")
 	if b=="1":
 		a=0
 		for i in range (1,11): 
-			word=choice(country)
-			print(word)
+			word=choice(country) #выбираем случайное значение из списка
+			print()
 			ans=input(str(i)+". Siseta "+word+" pealinn >>> ")
-			realans=Capitals.get(word)
-			if ans.title()==realans:
+			realans=Capitals.get(word) #делаем правильный ответ переменной
+			if ans.title()==realans: #проверяем правильность ответа
 				print("Õige!")
-				a+=1
+				a+=1 #если ответ правильный к переменной а добавляем 1
 			else:
 				print("Vale!")
-		result=a*100/10
+		result=a*100/10 #расчет результата
 	elif b=="2":
 		a=0
 		for i in range (1,11):
 			word=choice(capitals)
 			ans=input(str(i)+". Siseta mis riig pealinn on "+word+" >>> ")
-			realans=findkey(word,Capitals)
+			realans=findkey(word,Capitals) #использую функцию чтобы найти ключ зная значение
 			if ans.title()==realans:
 				print("Õige!")
 				a+=1
@@ -72,7 +72,7 @@ def testingknoledge(Capitals:dict)->float:
 		a=0
 		for i in range (1,16):
 			word=choice(country)
-			for key, value in Capitals.items(): #проверяем каждое значение вместе с ключами
+			for key, value in Capitals.items(): #проверяем каждое значение вместе с ключами чтобы опредилить даем мы столицу(значение) или страну(ключ) схожа с функцией поиска слова в словаре
 				if word==value:
 					realans=key
 					ans=input(str(i)+"Siseta riig kus pealinn on "+word+" >>> ")
@@ -86,13 +86,13 @@ def testingknoledge(Capitals:dict)->float:
 				a+=1
 			else:
 				print("Vale!")
-		result=a*100/10
+		result=a*100/15
 	else:
-		print("Vale funktsioon")
+		print("Vale funktsioon") #если была выбрана не одна из 3 функций то выдаем ошибку 
 	return result
 	
 def findkey(val:str,Capitals:dict)->str:
-	for key, value in Capitals.items():
-		if val == value:
-			return key
+	for key, value in Capitals.items(): # создаем цикл в котором проверяем значения
+		if val == value: # если значение совподает с переменной
+			return key # возращаем ключ
 	return "ei ole"
